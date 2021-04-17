72a7cc23d8cc911c03aeb925f38e96e2b1e3790c
Sayo Ugbeshe <esbeety@gmail.com>
2021-04-17 20:35:51 +0100

    Cleanup

--- commits.sh
+++ commits.sh
@@ -1,5 +1,4 @@
-# a million commits
-for Y in {2002..2025}
+for Y in {2001..2025}
 do
   mkdir $Y
   cd $Y
@@ -11,11 +10,14 @@ do
     do
       mkdir $D
       cd $D
-      for i in {01..12}
+      R=$(( ( RANDOM % 20 )  + 1 ))
+      for i in $(seq 1 $R)
       do
-        export GIT_AUTHOR_DATE="$Y-$M-$D $i:00:00" 
-        export GIT_COMMITTER_DATE="$Y-$M-$D $i:00:00" 
-        git commit --allow-empty -m "$i on $M $D $Y"
+        echo "$i on $M/$D/$Y" > commit.md
+        export GIT_COMMITTER_DATE="$Y-$M-$D 12:$i:00"
+        export GIT_AUTHOR_DATE="$Y-$M-$D 12:$i:00"
+        git add commit.md -f
+        git commit --date="$Y-$M-$D 12:0$i:00" -m "$i on $M $D $Y"
       done
       cd ../
     done
@@ -23,4 +25,7 @@ do
   done
   cd ../
 done
+git push origin master
+git rm -rf 20**
+git commit -am "cleanup"
 git push origin master
